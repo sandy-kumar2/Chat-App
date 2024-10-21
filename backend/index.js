b8707ca923f6573341236877919cd38c3698ae0b
sandy-kumar2 <imsandeep721@gmail.com>
2024-10-21 13:45:34 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -7,8 +7,8 @@ import messageRoute from "./routes/messageRoute.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import { app,server } from "./socket/socket.js";
-dotenv.config({});
 
+dotenv.config({});
  
 const PORT = process.env.PORT || 5000;
 
@@ -16,17 +16,16 @@ const PORT = process.env.PORT || 5000;
 app.use(express.urlencoded({extended:true}));
 app.use(express.json()); 
 app.use(cookieParser());
+
 const corsOption={
     origin:'http://localhost:3000',
     credentials:true
 };
 app.use(cors(corsOption)); 
 
-
 // routes
 app.use("/api/v1/user",userRoute); 
 app.use("/api/v1/message",messageRoute);
- 
 
 server.listen(PORT, ()=>{
     connectDB();
